9fbaf4bb6c12425eb3340411568d48a484e2c2f9
Giuseppe Fragalà <gmfdev2000@hotmail.com>
2017-08-25 22:44:36 +0200

    Set behavior. Everything looks ok

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ var app = express();
 var path = require('path');
 var jade = require('jade');
 
+//-------------------------------------------------------------------------------------
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = process.env.MONGODB_URI;      
+//(Focus on This Variable)
+//---------------------------------------------------------------------------------------
+
+
 app.set('views', __dirname + '/');
 app.set('view engine', 'jade');
 
@@ -11,21 +26,85 @@ app.get('/', function(req, res) {
 });
 
 
-app.get('/:id*', function(req, res) {
 	var input;
 	var original_url;
+	var rnd_short;
 	var short_url;
-	var rnd_short = Math.random() * (9999 -1001)/1001;
+
+
+
+app.get('/new/:id*', function(req, res) {
 
 	input = req.params.id;
-	original_url = "https://natural.com";
-	short_url = "https://short.com/" + rnd_short;
+	original_url = req.param('id') + req.param(0);
+ 	rnd_short= 1000 + Math.floor(Math.random() * (9999 -1001) + 1);
+	short_url = "https://freecodecamp-url-shortener-microservice.glitch.me/" + rnd_short;
 
 	var output = '{ "original_url": ' + '"' + original_url + '"' + ', "short_url": ' + '"' + short_url + '" }';
   	res.render('index', { title: 'FCC URL Shortener', head: "OUTPUT", message: output });
 
+	//---------------------------------------------------------------------------------------
+	// Use connect method to connect to the Server
+	  MongoClient.connect(url, function (err, db) {
+	  if (err) {
+	    console.log('Unable to connect to the mongoDB server. Error:', err);
+	  } else {
+	    console.log('Connection established to', url);
+
+	    // do some work here with the database.
+	    	var urlCollection = db.collection('urlCollection');
+	    	console.log("Scrivo un record");
+	    	urlCollection.insert({ _id: rnd_short, "original_url" : original_url, "short_url" : short_url});
+	    //Close connection
+	    db.close();
+	  }
+	});
+	//--------------------------------------------------------------------------------------
+
     //res.send(output);
 });
+
+
+app.get('/:id', function(req, res) {
+
+	if(!(input === "favicon.ico")){
+
+
+	var redirect_url = "";
+
+	//---------------------------------------------------------------------------------------
+	// Use connect method to connect to the Server
+	MongoClient.connect(url, function (err, db) {
+		if (err) {
+			console.log('Unable to connect to the mongoDB server. Error:', err);
+		} else {
+		    console.log('Connection established to', url);
+
+		    // do some work here with the database.
+		    	var urlCollection = db.collection('urlCollection');
+		    	var input = parseInt(req.params.id);
+		    	var field = "_id";
+				var query = {};
+				query[field] = input;
+		    	urlCollection.findOne( {query}, {"_id": 0, "original_url": 1}, function(error, doc){
+		    		if (err){res.send("Error:" + error)}
+		    		if (doc){	
+	  						redirect_url = doc.original_url;
+	  						res.redirect(redirect_url);
+		    		}else{res.send('{"error":"this URL is not on the database."}')}
+		    	});
+    		//Close connection
+			db.close();
+		}
+	});
+	//--------------------------------------------------------------------------------------
+
+  	//res.redirect(redirect_url);
+
+  }
+  
+});
+
 
 
 app.listen(process.env.PORT || 3000)
